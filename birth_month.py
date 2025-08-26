68129cdcf8df209969d690bcd30bc3669023aa94
jacob <email@>
2025-08-25 21:49:58 -0500

    part 1 -adding name, birthday month and letters in name. part 2 creating a class list

---
+++ birth_month.py
@@ -0,0 +1,25 @@
+# Part 1: Hello, birthday month - variable and if-statements
+#
+# Write a program that asks for your name and the month you were born in.
+#
+# Then, your program should print
+# A greeting to you, using your name
+# A message with the number of letters in your name
+# A 'Happy birthday month!' message if you were born in the current month
+# Easier - compare the user's input to "January" or "August" or whatever the current month is
+# Harder - use Python to figure out the current month and use that in the comparison. Check out the datetime library.
+
+name = input('Hello, please enter your name: ')
+birth_month = input('Please enter your birth month: ')
+
+print(f'Hello {name}')
+
+letters_in_name = len(name)
+
+print(f'You have + {letters_in_name} + letters in your name')
+
+if birth_month.lower() == 'august':
+    print('Happy birthday month!!!')
+else:
+    print('it is not your birthday month')
+
---
+++ class_list.py
@@ -0,0 +1,18 @@
+# Part 2: List of classes - lists and loops
+#
+# Write a program that asks for the names of all of the classes you are taking this semester
+# Save these class names in a list.
+# Print all the items in the list, one per line.
+
+num_classes = int(input('How many classes do you have? '))
+
+classes = []
+
+
+for i in range(num_classes):
+    class_name = input(f'Enter the name of class #{i + 1}: ')
+    classes.append(class_name)
+
+print('\nYour classes this semester:')
+for course in classes:
+    print(course)
